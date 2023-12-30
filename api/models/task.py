09f887a8c28ce87f20b814ad39d04511e7ef6d21
hisayoshi.horioka <hisayoshi.horioka@j-tech.jp>
2023-12-30 23:39:37 +0900

    chapter 13 completed.

--- api/models/task.py
+++ api/models/task.py
@@ -12,10 +12,10 @@ class Task(Base):
 
     done = relationship("Done", back_populates="task", cascade="delete")
 
+
 class Done(Base):
     __tablename__ = "dones"
 
     id = Column(Integer, ForeignKey("tasks.id"), primary_key=True)
 
     task = relationship("Task", back_populates="done")
-    
