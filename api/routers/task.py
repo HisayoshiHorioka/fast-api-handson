edaa78a96a103e8e405b227ffa193ab6858aa681
hisayoshi.horioka <hisayoshi.horioka@j-tech.jp>
2023-12-30 22:09:30 +0900

    initial commit.

---
+++ api/routers/task.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+import api.schemas.task as task_schema
+import api.cruds.task as task_crud
+from api.db import get_db
+
+router = APIRouter()
+
+
+@router.get("/tasks", response_model=list[task_schema.Task])
+async def list_tasks():
+    return [task_schema.Task(id=1, title="1つ目のTodoタスク")]
+
+
+@router.post("/tasks", response_model=task_schema.TaskCreateResponse)
+async def create_tasks(task_body: task_schema.TaskCreate, db: Session = Depends(get_db)):
+    return task_crud.create_task(db, task_body)
+
+
+@router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
+async def update_tasks(task_id: int, task_body: task_schema.TaskCreate):
+    return task_schema.TaskCreateResponse(id=task_id, **task_body.dict())
+
+
+@router.delete("/tasks/{task_id}", response_model=None)
+async def delete_tasks(task_id: int):
+    return
