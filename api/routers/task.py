a69443a0aa9176827b279e6ec16a691d476aa165
hisayoshi.horioka <hisayoshi.horioka@j-tech.jp>
2023-12-30 23:00:48 +0900

    Chapter 12 completed.

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 import api.schemas.task as task_schema
@@ -9,20 +9,28 @@ router = APIRouter()
 
 
 @router.get("/tasks", response_model=list[task_schema.Task])
-async def list_tasks():
-    return [task_schema.Task(id=1, title="1つ目のTodoタスク")]
+async def list_tasks(db: Session = Depends(get_db)):
+    return task_crud.get_tasks_with_done(db)
 
 
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
-async def create_tasks(task_body: task_schema.TaskCreate, db: Session = Depends(get_db)):
+async def create_task(task_body: task_schema.TaskCreate, db: Session = Depends(get_db)):
     return task_crud.create_task(db, task_body)
 
 
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
-async def update_tasks(task_id: int, task_body: task_schema.TaskCreate):
-    return task_schema.TaskCreateResponse(id=task_id, **task_body.dict())
+async def update_task(task_id: int, task_body: task_schema.TaskCreate, db: Session = Depends(get_db)):
+    task = task_crud.get_task(db, task_id=task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    
+    return task_crud.update_task(db, task_body, original=task)
 
 
 @router.delete("/tasks/{task_id}", response_model=None)
-async def delete_tasks(task_id: int):
-    return
+async def delete_task(task_id: int, db: Session = Depends(get_db)):
+    task = task_crud.get_task(db, task_id=task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    
+    return task_crud.delete_task(db, original=task)
